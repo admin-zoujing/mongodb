c24428c28c5578e57e3e91fb814fb1d2c073c8cc
admin-zoujing <489548322@qq.com>
2019-03-25 08:45:32 +0800

    Update init-mongodb.sh

--- MONGODB/init-mongodb.sh
+++ MONGODB/init-mongodb.sh
@@ -24,24 +24,24 @@ groupadd mongodb
 useradd -g mongodb -s /sbin/nologin mongodb
 cd $sourceinstall
 mkdir -pv /usr/local/mongodb
-tar -zxvf mongodb-linux-x86_64-rhel70-3.6.9.tgz -C /usr/local/mongodb
-mkdir -pv /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/{data,logs}
+tar -zxvf mongodb-linux-x86_64-rhel70-4.0.6.tgz -C /usr/local/mongodb
+mkdir -pv /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/{data,logs}
 chown -R mongodb:mongodb /usr/local/mongodb
 
 
 #2:configure配置安装
-#touch /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/logs/mongodb.log
-#/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/bin/mongod --dbpath=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/ --logpath=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/logs/mongodb.log --fork
+#touch /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/logs/mongodb.log
+#/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/bin/mongod --dbpath=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/ --logpath=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/logs/mongodb.log --fork
 #CentOS7 内存的设置方法为：systemctl set-property mongod1 MemoryLimit=10G
 #echo 3 > /proc/sys/vm/drop_caches
-cat > /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongodb.conf <<EOF
-dbpath=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/
+cat > /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongodb.conf <<EOF
+dbpath=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/
 journal=true
 directoryperdb=true
 wiredTigerDirectoryForIndexes=true
 #wiredTigerCacheSizeGB=3
 #这个数字是你设置的(limit-1G)*0.5,最小1.5G。
-logpath=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/logs/mongodb.log
+logpath=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/logs/mongodb.log
 logappend=true
 port=27017
 fork=true
@@ -50,17 +50,17 @@ bind_ip=0.0.0.0
 #replSet=rs0
 #oplogSize=10240
 #clusterAuthMode=keyFile
-#keyFile=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/testKeyFile.file  
+#keyFile=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/testKeyFile.file  
 maxConns=20000
 EOF
-chmod 755 /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongodb.conf
-echo 'export PATH=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/bin:$PATH' > /etc/profile.d/mongodb.sh 
+chmod 755 /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongodb.conf
+echo 'export PATH=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/bin:$PATH' > /etc/profile.d/mongodb.sh 
 source /etc/profile.d/mongodb.sh 
 chown -R mongodb:mongodb /usr/local/mongodb
 
 #3：服务随机启动
-#/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/bin/mongod -f /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongo.conf
-#echo '/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/bin/mongod -f /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongo.conf' >> /etc/rc.d/rc.local
+#/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/bin/mongod -f /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongo.conf
+#echo '/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/bin/mongod -f /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongo.conf' >> /etc/rc.d/rc.local
 #chmod +x /etc/rc.d/rc.local
 cat > /usr/lib/systemd/system/mongodb.service <<EOF
 [Unit]
@@ -71,7 +71,7 @@ After=syslog.target network.target
 Group=mongodb
 User=mongodb
 Type=forking
-ExecStart=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/bin/mongod -f /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongodb.conf
+ExecStart=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/bin/mongod -f /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongodb.conf
 LimitNOFILE=64000
 TimeoutStartSec=180
 PrivateTmp=true
@@ -98,7 +98,7 @@ firewall-cmd --permanent --query-port=27017/tcp
 firewall-cmd --reload
 
 #4：客户端连接验证
-#/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/bin/mongo
+#/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/bin/mongo
 ps aux |grep mongodb
 sleep 10
 cd
@@ -107,7 +107,7 @@ rm -rf $sourceinstall
 echo 'db.createUser({user:"admin",pwd:"Adminqwe123",roles:[{role:"root",db:"admin"},{role:"clusterAdmin",db:"admin"}]})' | mongo admin
 
 # echo 'db.dropUser("admin")' | mongo admin
-sed -i 's|#auth=true|auth=true|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongodb.conf
+sed -i 's|#auth=true|auth=true|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongodb.conf
 systemctl restart mongodb.service 
 
 #cgexec -g memory:DBLimitedGroup 限制内存
@@ -124,15 +124,15 @@ systemctl restart mongodb.service
 #恢复集合：mongorestore -u username -p password -h 127.0.0.1:27017 -d dbname -c collection --dir=<directory-name>  
 
 #设置副本集集群密码认证:
-#openssl rand -base64 756 > /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/testKeyFile.file
-#chmod 400 /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/testKeyFile.file
-#scp -P22 /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/testKeyFile.file root@192.168.8.51:/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/
-#scp -P22 /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/testKeyFile.file root@192.168.8.52:/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/
+#openssl rand -base64 756 > /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/testKeyFile.file
+#chmod 400 /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/testKeyFile.file
+#scp -P22 /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/testKeyFile.file root@192.168.8.51:/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/
+#scp -P22 /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/testKeyFile.file root@192.168.8.52:/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/
 
-#sed -i 's|#replSet=rs0|replSet=rs0|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongodb.conf
-#sed -i 's|#keyFile=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/testKeyFile.file|keyFile=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/data/testKeyFile.file|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongodb.conf
-#sed -i 's|#clusterAuthMode=keyFile|clusterAuthMode=keyFile|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongodb.conf
-#sed -i 's|#oplogSize=10240|oplogSize=10240|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-3.6.9/mongodb.conf
+#sed -i 's|#replSet=rs0|replSet=rs0|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongodb.conf
+#sed -i 's|#keyFile=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/testKeyFile.file|keyFile=/usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/data/testKeyFile.file|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongodb.conf
+#sed -i 's|#clusterAuthMode=keyFile|clusterAuthMode=keyFile|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongodb.conf
+#sed -i 's|#oplogSize=10240|oplogSize=10240|' /usr/local/mongodb/mongodb-linux-x86_64-rhel70-4.0.6/mongodb.conf
 
 
 #oplogSize=10G  
@@ -152,10 +152,15 @@ systemctl restart mongodb.service
     
 #Replica Set集群的备份：针对全库    
 #1每天晚上备份：mongodump -u admin -p Adminqwe123 -h "rs0/192.168.8.50,192.168.8.51" --oplog -o mongodbback
-#2立即导出日志：mongodump -u admin -p Adminqwe123 -h "rs0/192.168.8.50,192.168.8.51" -d local -c oplog.rs -o mongodbback/config
+#2立即导出日志(去掉认证)：mongodump -h "rs0/192.168.8.50,192.168.8.51" -d local -c oplog.rs -o mongodbback/config
 #3找到发生误删除的时间点
-#bsondump oplog.rs.bson |egrep "\"op\":\"d\"\,\"ns\":\"test\.oplog\"" |head -1 "t":1515379110,"i":1
+#bsondump mongodbback/config/local/oplog.rs.bson |egrep "\"op\":\"d\"\,\"ns\":\"test\.oplog\"" |head -1 "t":1553238789,"i":1
 #4复制oplog到备份目录
-#cp  mongodbback/config/oplog/oplog.rs.bson   mongodbback/config/oplog.bson
+#cp  mongodbback/config/local/oplog.rs.bson   mongodbback/oplog.bson
 #5进行恢复，添加之前找到的误删除的点（limt）
-#mongorestore -u admin -p Adminqwe123 --oplogReplay --oplogLimit "1515379110:1"  mongodbback/config
+#mongorestore -u admin -p Adminqwe123 --oplogReplay --oplogLimit "1553234987:1"  mongodbback
+
+
+
+
+
