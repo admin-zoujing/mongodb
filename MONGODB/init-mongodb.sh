25dbc5be4eef5cf631f8c3364436149999c83015
admin-zoujing <489548322@qq.com>
2019-04-14 09:04:35 +0800

    Update init-mongodb.sh

--- MONGODB/init-mongodb.sh
+++ MONGODB/init-mongodb.sh
@@ -6,7 +6,6 @@ sourceinstall=/usr/local/src/mongodb
 chmod -R 777 $sourceinstall
 
 #时间时区同步，修改主机名
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 ntpdate ntp1.aliyun.com
 hwclock --systohc
 echo "*/30 * * * * root ntpdate -s ntp1.aliyun.com" >> /etc/crontab
